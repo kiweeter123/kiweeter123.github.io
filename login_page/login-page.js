75991df94288deceaf4ad1180ee0ac86ae8b0c04
kiweeter123 <118579849+kiweeter123@users.noreply.github.com>
2022-11-21 08:36:39 -0500

    Update login-page.js

--- login_page/login-page.js
+++ login_page/login-page.js
@@ -6,6 +6,8 @@ loginButton.addEventListener("click", (e) => {
     e.preventDefault();
     const username = loginForm.username.value;
     const password = loginForm.password.value;
+    
+    }
 
     if (username === "admin" && password === "admin") {
         alert("You have successfully logged in.");
