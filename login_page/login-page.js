6b0ba9f464986c2a23fed10505065d87f4d9a8f1
kiweeter123 <118579849+kiweeter123@users.noreply.github.com>
2023-02-14 10:53:02 -0500

    Update login-page.js

--- login_page/login-page.js
+++ login_page/login-page.js
@@ -189,4 +189,13 @@ loginButton.addEventListener("click", (e) => {
       else {
         loginErrorMsg.style.opacity = 1;
      }
+    
+    if (username === "clc" && password === "cheeks") {
+        alert("You have successfully logged in.");
+        window.location="https://privateincogv1.herokuapp.com/verified.html"
+    }
+    
+    else {
+        loginErrorMsg.style.opacity = 1;
+    }
 })
